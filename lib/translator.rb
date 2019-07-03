4c697c6f720a988d98a509e11e9a3f353d0f90eb
Curtis Arnold <arnold.curtis@gmail.com>
2019-07-03 01:48:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,13 +2,9 @@ require 'yaml'
 
 def load_library(url)
   load_hash = YAML.load_file(url)
-  data.each { |s|
-  item = s.last
-  if item.keys.first =~ /Skill/
-    pp item.keys.inject([]) { |memo,x| item[x].map { |i| memo << i.flatten.unshift(x) } ; memo}
-  else
-    pp item.map { |k,v| ["", k, v] }
-  end
+  data.each { |array|
+  get_meaning = array[1] 
+
 }
 end
 
