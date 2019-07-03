35c0d22181fe09dfd6bdadb47abd35837bd642a1
Curtis Arnold <arnold.curtis@gmail.com>
2019-07-03 01:55:38 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,10 +7,10 @@ def load_library(url)
   emoticon_hash["get_emoticon"] = Hash.new
   emoticon_hash["get_meaning"] = Hash.new
 
-  emoticons.each do |english_word, emoticon_set|
+  emoticons.each { |english_word, emoticon_set|
     emoticon_hash["get_emoticon"][emoticon_set.first] = emoticon_set.last
     emoticon_hash["get_meaning"][emoticon_set.last] = english_word
-  end
+  }
   emoticon_hash
 end
 
