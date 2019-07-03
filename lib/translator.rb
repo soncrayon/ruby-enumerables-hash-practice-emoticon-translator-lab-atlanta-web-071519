4d579dbb28350560a6ee9660df848acc5714663e
Curtis Arnold <arnold.curtis@gmail.com>
2019-07-03 01:42:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,16 +3,14 @@ require 'yaml'
 def load_library(url)
   load_hash = YAML.load_file(url)
   subject = data["Subject_list"]
-
-require 'pp'
-subject.each do |s|
+  data.each { |s|
   item = s.last
   if item.keys.first =~ /Skill/
     pp item.keys.inject([]) { |memo,x| item[x].map { |i| memo << i.flatten.unshift(x) } ; memo}
   else
     pp item.map { |k,v| ["", k, v] }
   end
-end
+}
 end
 
 def get_japanese_emoticon
